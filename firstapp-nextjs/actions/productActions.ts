a51b7ac83606d6e0e4e8e90bf39608410ba136f8
Oscar (Van NGUYEN) <32070259+uwerrrr@users.noreply.github.com>
2024-03-21 15:02:09 +1100

    Update productActions.ts

--- firstapp-nextjs/actions/productActions.ts
+++ firstapp-nextjs/actions/productActions.ts
@@ -3,30 +3,6 @@ import { Product } from "@/interfaces";
 import { FormDataSchema } from "@/schemas/FormDataSchema";
 import { revalidatePath, revalidateTag } from "next/cache";
 
-// export const addProductToDb = async (e: FormData) => {
-//   const product = e.get("product")?.toString();
-//   const price = e.get("price")?.toString();
-
-//   if (!product || !price) return;
-
-//   const newProduct: Product = {
-//     product: product,
-//     price: price,
-//   };
-
-//   // POST request
-//   await fetch("https://65fa6ab13909a9a65b1a5bee.mockapi.io/products", {
-//     method: "POST",
-//     body: JSON.stringify(newProduct),
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
-//   });
-
-//   // revalidation
-//   revalidateTag("products"); // re-fetch any fetch that has tag "products"
-//   // revalidatePath('/') // re-fetch all fetch in a path
-// };
 
 export const addProductToDb = async (state: any, FormData: FormData) => {
   const validatedFields = FormDataSchema.safeParse({
